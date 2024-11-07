1e4827a6d2b1c93fbd190dd36516c3235ebd3c85
Skunk-XCP <skunk-xcp@hotmail.fr>
2024-11-07 10:01:19 +0100

    Initial commit for back-end

---
+++ pennywise/src/test/java/com/pennywise/pennywise/PennywiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pennywise.pennywise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennywiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
